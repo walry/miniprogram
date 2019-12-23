28e6ac1bfa498c3d709ad81f477cc569c0d05557
杜政伟 <duzhengwei@jiandan100.cn>
2019-12-23 15:38:05 +0800

    save

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -41,7 +41,7 @@ class Index extends Component {
     return (
       <View className='index'>
         <AtAvatar circle size='large' openData={opendata}></AtAvatar>
-        <View className='description'><Text >欢迎进入量化交易小程序！</Text></View>
+        <View className='description'><Text >欢迎进入小程序！</Text></View>
         <Button type='primary' onClick={this.handleButtonClick.bind(this)}>点击进入</Button>
       </View>
     )
